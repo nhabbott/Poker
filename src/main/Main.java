7db6afd69df64ad84dc6ffe214f4258f3f3df6f5
Noah <nabbott2@lamar.edu>
2020-04-12 19:30:43 -0500

    Finished #1
    Check #1 for more information.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -15,7 +16,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import javafx.animation.Animation:
+import javafx.util.Duration;
+import javafx.animation.Animation;
+import javafx.animation.FadeTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.SequentialTransition;
 
 public class Main extends Application {
 
@@ -146,6 +152,60 @@ public class Main extends Application {
 		// Set container content to center and return it
 		settings.setAlignment(Pos.CENTER);
 		return settings;
+	}
+	
+	// Animation for "big win"
+	// Make sure the text for the win amount and the circle have their opacity set to 0.0f
+	private SequentialTransition bigWinAnimation(Node circle, Node text) { 
+		// Fade in the circle and make the text visible
+		FadeTransition fadeIn = new FadeTransition();
+		fadeIn.setFromValue(0.0f);
+		fadeIn.setToValue(1.0f);
+		fadeIn.setCycleCount(1);
+		fadeIn.setNode(circle);
+		fadeIn.setDuration(Duration.seconds(3));
+		fadeIn.setOnFinished(e -> {
+			text.setOpacity(1.0f);
+		});
+		
+		// Emphasize the win amount
+		ScaleTransition scale = new ScaleTransition();  
+        scale.setByX(1.5f);  
+        scale.setByY(1.2f);  
+        scale.setCycleCount(4);  
+        scale.setAutoReverse(true);  
+        scale.setNode(text);
+        scale.setDuration(Duration.seconds(1));
+		
+		// Spin the win amount around twice
+		RotateTransition rotateTxt = new RotateTransition();
+		rotateTxt.setFromAngle(0f);
+		rotateTxt.setToAngle(360f);
+		rotateTxt.setCycleCount(2);
+		rotateTxt.setAutoReverse(true);
+		rotateTxt.setNode(text);
+		rotateTxt.setDuration(Duration.seconds(1.5f));
+		
+		// Make the text disappear
+		FadeTransition fadeOutTxt = new FadeTransition();
+		fadeOutTxt.setFromValue(1.0f);
+		fadeOutTxt.setToValue(0.0f);
+		fadeOutTxt.setCycleCount(1);
+		fadeOutTxt.setNode(text);
+		fadeOutTxt.setDuration(Duration.seconds(2));
+		
+		// Make circle disappear
+		FadeTransition fadeOutCirc = new FadeTransition();
+		fadeOutCirc.setFromValue(1.0f);
+		fadeOutCirc.setToValue(0.0f);
+		fadeOutCirc.setCycleCount(1);
+		fadeOutCirc.setNode(circle);
+		fadeOutCirc.setDuration(Duration.seconds(2));
+		
+		// Transition that contains all of the needed transitions; to be returned
+		SequentialTransition ret = new SequentialTransition(fadeIn, scale, rotateTxt, fadeOutTxt, fadeOutCirc);
+		
+		return ret;
 	}
 	
 	// Launches the JavaFX application
